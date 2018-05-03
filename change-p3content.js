9f2b135fdda5ace633a13596106493538c728296
zhangbo <zhangbojoke@gmail.com>
2018-05-03 18:30:27 +0800

    update png

--- change-p3content.js
+++ change-p3content.js
@@ -18,37 +18,12 @@
     4: {
       gif: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/huhehaote.gif',
       btn: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/huhehaote-btn.png',
-      p3Line: ['http://p3.ifengimg.com/29b92e35b2b20708/2016/52/hm1.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/hm2.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/hm3.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/hm4.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/hm5.png']
+      p3Line: ['./gs-4-1.png', './gs-4-2.png']
     },
     5: {
       gif: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/xian.gif',
       btn: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/xian-btn.png',
-      p3Line: ['http://p3.ifengimg.com/29b92e35b2b20708/2016/52/qq1.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/qq2.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/qq3.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/qq4.png']
-    },
-    6: {
-      gif: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/chengdu.gif',
-      btn: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/chengdu-btn.png',
-      p3Line: ['http://p3.ifengimg.com/29b92e35b2b20708/2016/52/mj1.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/mj2.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/mj3.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/mj4.png']
-    },
-    7: {
-      gif: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/shenzhen.gif',
-      btn: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/shenzhen-btn.png',
-      p3Line: ['http://p3.ifengimg.com/29b92e35b2b20708/2016/52/hysx1.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/hysx2.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/hysx3.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/hysx4.png']
-    },
-    8: {
-      gif: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/xiamen.gif',
-      btn: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/xiamen-btn.png',
-      p3Line: ['http://p3.ifengimg.com/29b92e35b2b20708/2016/52/mny1.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/mny2.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/mny3.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/mny4.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/mny5.png']
-    },
-    9: {
-      gif: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/shenyang.gif',
-      btn: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/shenyang-btn.png',
-      p3Line: ['http://p3.ifengimg.com/29b92e35b2b20708/2016/52/erz1.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/erz2.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/erz3.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/erz4.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/erz5.png']
-    },
-    10: {
-      gif: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/shaoxing.gif',
-      btn: 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/shaoxing-btn.png',
-      p3Line: ['http://p3.ifengimg.com/29b92e35b2b20708/2016/52/wpc1.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/wpc2.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/wpc3.png', 'http://p3.ifengimg.com/29b92e35b2b20708/2016/52/wpc4.png']
+      p3Line: ['./gs-5-1.png', './gs-4-2.png']
     }
   }
 })(jQuery)
